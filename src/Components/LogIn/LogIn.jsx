eecedf6031b9fb384319891fb930aaf0af4aa97d
abida-sultana-ake <151779423+abida-sultana-ake@users.noreply.github.com>
2025-04-30 20:57:11 +0600

    commit

---
+++ src/Components/LogIn/LogIn.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const LogIn = () => {
+  return (
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+      <div className="card-body">
+        <form className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
---
+++ src/Firebase/Firebase.int.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_ruqBftYnmWSn83dhGa8Vfgpd7gT2xKY",
+  authDomain: "my-another-project-2e5ff.firebaseapp.com",
+  projectId: "my-another-project-2e5ff",
+  storageBucket: "my-another-project-2e5ff.firebasestorage.app",
+  messagingSenderId: "1060219539476",
+  appId: "1:1060219539476:web:b66f9cba125ba5b34ed701"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
